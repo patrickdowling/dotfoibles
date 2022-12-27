3b4355ecfbf8e8305d8beaf93110bd996f9804b7
Patrick Dowling <pld@gurkenkiste.com>
2022-12-27 19:32:38 +0100

    Update theme URLs

--- packages/themes/install.sh
+++ packages/themes/install.sh
@@ -2,12 +2,12 @@
 
 DOT_THEMES=$HOME/.local/share/themes
 
-dot_install_directory $DOT_THEMES
+dot_install_directory "$DOT_THEMES"
 
-dot_git_clone "fnune/base16-fzf" "$DOT_THEMES"
+dot_git_clone "tinted-theming/base16-fzf" "$DOT_THEMES"
 
 if [ "$DOT_OS" = "macos" ] ; then
-	dot_git_clone "martinlindhe/base16-iterm2" "$DOT_THEMES"
+	dot_git_clone "tinted-theming/base16-iterm2" "$DOT_THEMES"
 elif [ "$DOT_OS" = "linux" ] ; then
 	dot_git_clone "aarowill/base16-gnome-terminal" "$DOT_THEMES"
 fi
