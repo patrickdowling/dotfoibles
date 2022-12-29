b461aca51dc8a3cb746577ba701fc2d2bb0c206b
Patrick Dowling <pld@gurkenkiste.com>
2022-12-29 16:25:10 +0100

    Port themes to ansible

--- packages/nvim/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# set -x
-
-NVIM_CONFIG_DIR="$HOME/.config/nvim"
-#dot_install_symlink "$(realpath "./config_nvim")" "$NVIM_CONFIG_DIR"
-dot_install_directory "$NVIM_CONFIG_DIR"
-dot_install_symlink "$(realpath "./init.lua")" "$NVIM_CONFIG_DIR/init.lua"
-
-#pip3 install neovim
