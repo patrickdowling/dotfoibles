2949ffdbefa39304cc87fc36592b913401870ff4
Patrick Dowling <pld@gurkenkiste.com>
2023-01-12 16:48:16 +0100

    nvim: a few keymaps

--- packages/nvim/lua/core/keymaps.lua
+++ packages/nvim/lua/core/keymaps.lua
@@ -13,16 +13,32 @@ keymap("v", "J", ":m '>+1<CR>gv=gv")
 -- <F>ile <T>ree
 keymap('n', '<leader>ft', ':NvimTreeToggle<CR>')
 
--- Telescope
+-- <leader>Find (Telescope)
 local telescope_available, telescope = pcall(require, 'telescope.builtin')
 if telescope_available then
     keymap('n', '<leader>fb', telescope.buffers, {})
 
     keymap('n', '<leader>ff', telescope.find_files, {})
     keymap('n', '<leader>fg', telescope.live_grep, {})
+    keymap('n', '<leader>?', telescope.oldfiles)
+
+    keymap('n', '<leader>df', telescope.diagnostics)
 end
 
--- Diagnostics
+-- <leader>Diagnostics
 keymap('n', '[d', vim.diagnostic.goto_prev)
 keymap('n', ']d', vim.diagnostic.goto_next)
 keymap('n', '<leader>e', vim.diagnostic.open_float)
+
+local diagnostic_hide = function()
+    vim.diagnostic.hide(nil, 0)
+end
+
+local diagnostic_show = function()
+    vim.diagnostic.show(nil, 0)
+end
+
+keymap('n', '<leader>dh', diagnostic_hide) -- LSP Diagnostic Hide (for current buffer)
+keymap('n', '<leader>ds', diagnostic_show) -- LSP Diagnostic Show (for current buffer)
+
+-- NOTE There's a bunch of lsp per-buffer mappings set in lsp_config.lua
--- packages/nvim/lua/plugin_config/lsp_config.lua
+++ packages/nvim/lua/plugin_config/lsp_config.lua
@@ -22,6 +22,21 @@ local lsp_servers = {
     }
 }
 
+local on_attach = function(_, bufnr)
+    local nmap_buffer = function(keys, func, desc)
+        if desc then desc = 'LSP: ' .. desc end
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap_buffer('<leader>cf', vim.lsp.buf.format, '[C]ode [F]ormat')
+    nmap_buffer('<leader>cr', vim.lsp.buf.rename, '[C]ode [R]rename')
+
+    nmap_buffer('<leader>gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap_buffer('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+    nmap_buffer('K', vim.lsp.buf.hover, 'Hover definition')
+end
+
 local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup { ensure_installed = vim.tbl_keys(lsp_servers) }
@@ -29,7 +44,7 @@ mason_lspconfig.setup_handlers {
     function(server_name)
         require("lspconfig")[server_name].setup {
             -- capabilities
-            -- on_attach
+            on_attach = on_attach,
             settings = lsp_servers[server_name]
         }
     end
