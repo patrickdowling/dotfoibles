b9f546ac2251ec13c628b6e2bbee412bebb453b3
Patrick Dowling <pld@gurkenkiste.com>
2023-02-02 15:05:46 +0100

    Script to extract header paths for clangd

---
+++ bin/clang_includes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Helper for generating compile_flags.txt
+# clangd -- at least on OS X -- can't seem to find the relevant system headers and needs help
+
+clang++ -E -v -xc++ /dev/null 2>&1 | sed -n '/^#include </,/^End/p' | sed '1d;$d' | sed 's/^[ ]/-isystem\n/'
